b896c51e9a2358f2c7d655dba90a47ca7b20764f
Joao-789 <00001118510240sp@al.educacao.sp.gov.br>
2024-08-30 12:15:57 -0300

    Update and rename README.md to esboço.js

---
+++ esboço.js
@@ -0,0 +1,18 @@
+ configuração de função () {
+  createCanvas(600, 600);
+   background("white");
+}
+
+
+function draw() {
+  
+  stroke ("blue");
+  fill ("red");
+ 
+  
+  // console.log(mouseIsPressed);
+  
+  if (mouseIsPressed){
+    rect(mouseX, mouseY, 20, 35);
+  }
+}
